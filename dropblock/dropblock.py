096bed072ec76090406950a415b9e43d55f4d3fe
Eliza and black <55172223+Eliza-and-black@users.noreply.github.com>
2021-12-04 16:40:48 +0800

    Update dropblock.py
     Changes in this folk(only in DropBlock2D):
            1.Make gamma consistent with the original paper, and correlatively let the center of mask stay in the shaded green region of the original paper;
            2.Make each feature channel has its DropBlock mask as the original paper propose.

--- dropblock/dropblock.py
+++ dropblock/dropblock.py
@@ -23,7 +23,7 @@ class DropBlock2D(nn.Module):
        https://arxiv.org/abs/1810.12890
        
     Changes in this folk(only in DropBlock2D):
-        1.Make gamma consistent with the paper, and correlatively let the center of mask stay in the shaded green region of the original paper;
+        1.Make gamma consistent with the original paper, and correlatively let the center of mask stay in the shaded green region of the original paper;
         2.Make each feature channel has its DropBlock mask as the original paper propose.
 
     """
